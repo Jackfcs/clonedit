b66a17867c8f669cac810e34b377e17120dd50aa
JackCS <jackfcs22@gmail.com>
2021-09-27 18:41:53 +0100

    Nav bar styling complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import "../styles/Navbar.scss"
-import Input from "./Input"
-import UserDropdown from "./UserDropdown"
+import React from "react";
+import "../styles/Navbar.scss";
+import Input from "./Input";
+import UserDropdown from "./UserDropdown";
+import { AddOutline } from "react-ionicons";
+import Logo from "../icons/logo.png";
 
 const Navbar = () => {
-    return (
-        <div className="navbar-container">
-            <div>logo reddit</div>
-            <Input placeHolder="Search Reddit" />
-            <div>+</div>
-            <UserDropdown /> 
-        </div>
-    )
-}
+  return (
+    <div className="navbar-container">
+      <div className="logo-container">
+        <img className="main-logo" src={Logo} alt="Reddit logo"></img> reddit
+      </div>
+      <Input placeHolder="Search Reddit" />
+      <AddOutline
+        color={"#6D6D6E"}
+        height="28px"
+        width="28px"
+        cssClasses="add-post-cross"
+      />
 
-export default Navbar
+      <UserDropdown />
+    </div>
+  );
+};
+
+export default Navbar;
