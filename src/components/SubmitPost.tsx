4502bb54c9ad5101d502e4cbd6186906b719c2de
JackCS <jackfcs22@gmail.com>
2021-10-04 14:08:10 +0100

    Added styling for post submit

--- src/components/SubmitPost.tsx
+++ src/components/SubmitPost.tsx
@@ -4,19 +4,30 @@ import InfoPanels from "./InfoPanels";
 
 const SubmitPost: React.FC = () => {
   return (
-
     <div className="bg-padding">
       <div className="create-post-page">
         <div className="submit-post-section">
           <h3 className="create-post-title">Create a post</h3>
-          <div className="post-creation-container">post</div>
+          <div className="post-creation-container">
+            <div className="post-types">
+              <div className="text-post post-type">Post</div>
+              <div className="media-post post-type">Images</div>
+              <div className="link-post post-type">Link</div>
+            </div>
+            <div className="post-content">
+              <form className="post-form">
+                <input placeholder="Title" className="post-input" type="text"></input>
+                <textarea placeholder="Text (optional)" className="post-input"></textarea>
+                <input className="submit-post" type="submit" value="POST"></input>
+              </form>
+            </div>
+          </div>
         </div>
         <div className="create-post-info-panels">
           <InfoPanels />
         </div>
       </div>
     </div>
-   
   );
 };
 
